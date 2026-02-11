a37aef0ee716964caf0f3a3c7265cc43a0b9da9c
k1-karim <kawthark005@gmail.com>
2026-02-11 23:39:18 +0000

    edited haskell type in intro1

--- lab2-functionalPrimer/haskell/intro1.hs
+++ lab2-functionalPrimer/haskell/intro1.hs
@@ -1,28 +1,48 @@
 module Intro1 where
 
--- define named constants:
+-- define named constants edit by defining the type variable:
+r1 :: Int
 r1 = 1
+
+r2 :: Double
 r2 = b^2 + 1/b where b = 2
 
 -- define a function:
+diff :: Integer -> Integer -> Integer 
 diff a b = abs (a - b)
 
+r3 :: Integer
 r3 = diff (diff a a) a  where a = 1
+
+r4 :: Integer
 r4 = diff (diff a b) b  where a = 1; b = 1
 
 -- lists by enumeration
+r5 :: Integer
 r5 = [1,3,4,1]
+
+r6 :: Integer
 r6 = [1..5]
 
+inc :: Integer -> Integer
 inc n = n + 1
 
+r7 :: Integer
 r7 = map inc [1..3]
+
+r8 :: Integer
 r8 = map (diff 2) [1..3]
+
+r9 :: Double 
 r9 = map sqrt [1..3]
 
+r10 :: Double
 r10 = zip [1..3] (map sqrt [1..3])
+
+r11 :: Double
 r11 = zip [1..3] (map sqrt [1..2])
 
+r12 :: IO
 r12 = print [1..3]
 
 main :: IO ()
